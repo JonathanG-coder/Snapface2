52a88d232092cbafd1d12b7ea1ac6223abe9bbc4
Jonathan <jonathangager75@gmail.com>
2024-12-05 09:49:03 +0100

    Add Form Input Comments

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,26 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { Comment } from '../../../core/models/comment.model';
 import { MatCardModule } from '@angular/material/card';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Comment } from '../../../core/models/comment.model';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [MatListModule, CommonModule, MatCardModule],
+  imports: [
+    MatListModule,
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './comments.component.html',
-  styleUrl: './comments.component.scss'
+  styleUrls: ['./comments.component.scss'] // Attention : utilisez `styleUrls` au pluriel.
 })
 export class CommentsComponent implements OnInit {
 
   @Input() comments!: Comment[];
 
-  constructor() {}
+  commentCtrl!: FormControl;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-      
+    this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
   }
 
+  onLeaveComment() {
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './components/comments/comments.component';
 import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,11 +11,14 @@ import { MaterialModule } from './material.module';
   imports: [
     CommonModule,
     MaterialModule,
-    CommentsComponent
+    CommentsComponent,
+    ReactiveFormsModule
   ],
   exports: [
     CommentsComponent,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
+
   ]
 })
 export class SharedModule { }
