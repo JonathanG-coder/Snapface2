27182f984e81d096caa3ae46c61dbc56e42db4dd
Jonathan <jonathangager75@gmail.com>
2024-12-19 11:50:36 +0100

    Refactor toolbar layout to center elements with flexible spacing

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
     imports: [
         MatToolbarModule,
         RouterModule
